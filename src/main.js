3a52850edcf92e267f0f220f9697b5e5e3eab178
saezeric <saezescalonaeric@fpllefia.com>
2025-03-09 21:21:02 +0100

    Menu desktop terminado

--- src/main.js
+++ src/main.js
@@ -3,12 +3,3 @@
 import "./styles/bootstrap.scss";
 import "./styles/custom.scss";
 import "bootstrap";
-
-window.addEventListener("scroll", function () {
-  const header = document.querySelector(".navbar.bg-transparent.fixed-top");
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
